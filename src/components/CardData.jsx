070a74f5777f4b949b37c3355447689e80b2924b
Batanai Gwanyanya <batanai178@gmail.com>
2022-09-28 15:34:41 +0200

    first-commit

---
+++ src/components/CardData.jsx
@@ -0,0 +1,23 @@
+import pizza from "../assets/pizza.jpg"
+import burger from "../assets/burger.jpg"
+import coffee from "../assets/coffee.jpg"
+
+const CardData = [
+    {
+        img: pizza,
+        title: "Pizza",
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis a ipsa fuga rem ad saepe, modi id ab similique accusamus, cum doloremque nesciunt non voluptates vero labore magni nam dignissimos."
+    },
+    {
+        img: burger,
+        title: "Burger",
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis a ipsa fuga rem ad saepe, modi id ab similique accusamus, cum doloremque nesciunt non voluptates vero labore magni nam dignissimos."
+    },
+    {
+        img: coffee,
+        title: "Coffee",
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis a ipsa fuga rem ad saepe, modi id ab similique accusamus, cum doloremque nesciunt non voluptates vero labore magni nam dignissimos."
+    }
+];
+
+export default CardData;
